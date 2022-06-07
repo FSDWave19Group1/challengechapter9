237f218f8f6c5c986b5a1a4c110e4318d7057cd6
jivoryy <johannesbosch311@gmail.com>
2022-06-07 20:23:37 +0700

    completed objective

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import firebase from "./services/firebase";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "./components/layouts/default";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* <Route path="/" element={<Home />}></Route> */}
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        ></Route>
+        {/* <Route path="/userprofile" element={<UserProfile />}></Route> */}
+        {/* <Route path="/game/list" element={<GameList />}></Route> */}
+        {/* <Route path="/game/suit" element={<GameSuit />}></Route> */}
       </Routes>
     </Router>
   );
