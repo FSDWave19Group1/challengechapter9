a98e7aa0b57ced9d40a2db0b0b1177f02bacbf90
jivoryy <johannesbosch311@gmail.com>
2022-06-07 14:25:08 +0700

    create route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-
-import './App.css';
-import firebase from './services/firebase';
-import Register from './pages/Register';
-import Login from './pages/Login'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import firebase from "./services/firebase";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />}></Route> */}
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
