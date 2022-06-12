a3cd5e592ea5f7b88605b534553aa2e33cf16f28
Eko Indrawijaya <indrawijaya.echo@gmail.com>
2022-06-12 21:42:24 +0700

    update layout, update footer.css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,15 +6,12 @@ const Footer = (props) => {
     <div class="container py-4">
       <div class="row gy-4 gx-5">
         <div class="col-lg-4 col-md-6">
-          <h5 class="h1 text-white">FB.</h5>
-          <p class="small text-muted">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt.
-          </p>
+          <h5 class="h1 text-white">FSD19</h5>
+          <p class="small text-muted">This website host small indie games.</p>
           <p class="small text-muted mb-0">
-            &copy; Copyrights. All rights reserved.{" "}
+            &copy; Copyrights@2022. All rights reserved.{" "}
             <a class="text-primary" href="#">
-              Bootstrapious.com
+              FSD19T1
             </a>
           </p>
         </div>
@@ -22,33 +19,10 @@ const Footer = (props) => {
           <h5 class="text-white mb-3">Quick links</h5>
           <ul class="list-unstyled text-muted">
             <li>
-              <a href="#">Home</a>
+              <a href="/">Home</a>
             </li>
             <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Get started</a>
-            </li>
-            <li>
-              <a href="#">FAQ</a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-lg-2 col-md-6">
-          <h5 class="text-white mb-3">Quick links</h5>
-          <ul class="list-unstyled text-muted">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Get started</a>
-            </li>
-            <li>
-              <a href="#">FAQ</a>
+              <a href="/Profile">Profile</a>
             </li>
           </ul>
         </div>
@@ -63,12 +37,12 @@ const Footer = (props) => {
               <input
                 class="form-control"
                 type="text"
-                placeholder="Recipient's username"
+                placeholder="Your email"
                 aria-label="Recipient's username"
                 aria-describedby="button-addon2"
               />
               <button class="btn btn-primary" id="button-addon2" type="button">
-                <i class="fas fa-paper-plane"></i>
+                <p class="text-center"> Subscribe </p>
               </button>
             </div>
           </form>
